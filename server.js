ef1cfa5f97fbae7df2b3e730a3ee9e2dfd534f2d
ChrisVaz1995 <christopher.vazquez2015@gmail.com>
2021-09-14 14:16:18 -0500

    modified the server.js file and the index.html file

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ captureUncaught: true,
 captureUnhandledRejections: true
 });
 
+const students = [];
 const app = express();
 
 app.use(express.json());
@@ -16,6 +17,26 @@ app.use('/style', express.static('./public/styles.css'));
 app.get('/', (req,res)=> {
     res.sendFile(path.join(__dirname, '/public/index.html'));
     rollbar.info('HTML file served successfully');
+})
+
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    const index = students.findIndex(studentName=> studentName === name)
+
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Chris', type: 'manual entry'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
+
 })
 
 const port = process.env.PORT || 4545;
